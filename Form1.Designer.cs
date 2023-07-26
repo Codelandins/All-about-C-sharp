1c423dd839b6e2ba5b36ed410c464702c2259885
Mehmet KIROGLU <140586918+Codelandins@users.noreply.github.com>
2023-07-26 09:18:07 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace MenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.categoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rambo2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.romanticToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loveLikesCoincidensToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paranormalActivityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scifinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spacePassengersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coloursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickAccessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.categoriesToolStripMenuItem,
+            this.coloursToolStripMenuItem,
+            this.quickAccessToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1290, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // categoriesToolStripMenuItem
+            // 
+            this.categoriesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionToolStripMenuItem,
+            this.romanticToolStripMenuItem,
+            this.scaryToolStripMenuItem,
+            this.scifinToolStripMenuItem});
+            this.categoriesToolStripMenuItem.Name = "categoriesToolStripMenuItem";
+            this.categoriesToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.categoriesToolStripMenuItem.Text = "Categories";
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rambo2ToolStripMenuItem});
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.actionToolStripMenuItem.Text = "Action";
+            // 
+            // rambo2ToolStripMenuItem
+            // 
+            this.rambo2ToolStripMenuItem.Name = "rambo2ToolStripMenuItem";
+            this.rambo2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rambo2ToolStripMenuItem.Text = "Rambo 2";
+            this.rambo2ToolStripMenuItem.Click += new System.EventHandler(this.rambo2ToolStripMenuItem_Click);
+            // 
+            // romanticToolStripMenuItem
+            // 
+            this.romanticToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loveLikesCoincidensToolStripMenuItem});
+            this.romanticToolStripMenuItem.Name = "romanticToolStripMenuItem";
+            this.romanticToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.romanticToolStripMenuItem.Text = "Romantic";
+            // 
+            // loveLikesCoincidensToolStripMenuItem
+            // 
+            this.loveLikesCoincidensToolStripMenuItem.Name = "loveLikesCoincidensToolStripMenuItem";
+            this.loveLikesCoincidensToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.loveLikesCoincidensToolStripMenuItem.Text = "Love likes coincidens";
+            // 
+            // scaryToolStripMenuItem
+            // 
+            this.scaryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paranormalActivityToolStripMenuItem});
+            this.scaryToolStripMenuItem.Name = "scaryToolStripMenuItem";
+            this.scaryToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.scaryToolStripMenuItem.Text = "Scary";
+            // 
+            // paranormalActivityToolStripMenuItem
+            // 
+            this.paranormalActivityToolStripMenuItem.Name = "paranormalActivityToolStripMenuItem";
+            this.paranormalActivityToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.paranormalActivityToolStripMenuItem.Text = "Paranormal Activity";
+            // 
+            // scifinToolStripMenuItem
+            // 
+            this.scifinToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spacePassengersToolStripMenuItem});
+            this.scifinToolStripMenuItem.Name = "scifinToolStripMenuItem";
+            this.scifinToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.scifinToolStripMenuItem.Text = "Sci-fin";
+            // 
+            // spacePassengersToolStripMenuItem
+            // 
+            this.spacePassengersToolStripMenuItem.Name = "spacePassengersToolStripMenuItem";
+            this.spacePassengersToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.spacePassengersToolStripMenuItem.Text = "Space Passengers";
+            // 
+            // coloursToolStripMenuItem
+            // 
+            this.coloursToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blueToolStripMenuItem,
+            this.grayToolStripMenuItem,
+            this.orangeToolStripMenuItem});
+            this.coloursToolStripMenuItem.Name = "coloursToolStripMenuItem";
+            this.coloursToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.coloursToolStripMenuItem.Text = "Colours";
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // grayToolStripMenuItem
+            // 
+            this.grayToolStripMenuItem.Name = "grayToolStripMenuItem";
+            this.grayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.grayToolStripMenuItem.Text = "Gray";
+            this.grayToolStripMenuItem.Click += new System.EventHandler(this.grayToolStripMenuItem_Click);
+            // 
+            // orangeToolStripMenuItem
+            // 
+            this.orangeToolStripMenuItem.Name = "orangeToolStripMenuItem";
+            this.orangeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.orangeToolStripMenuItem.Text = "Orange";
+            this.orangeToolStripMenuItem.Click += new System.EventHandler(this.orangeToolStripMenuItem_Click);
+            // 
+            // quickAccessToolStripMenuItem
+            // 
+            this.quickAccessToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutUsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.quickAccessToolStripMenuItem.Name = "quickAccessToolStripMenuItem";
+            this.quickAccessToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.quickAccessToolStripMenuItem.Text = "Quick Access";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutUsToolStripMenuItem.Text = "About us";
+            this.aboutUsToolStripMenuItem.Click += new System.EventHandler(this.aboutUsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(151, 83);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(972, 430);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 525);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem categoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rambo2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem romanticToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loveLikesCoincidensToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paranormalActivityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scifinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spacePassengersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coloursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quickAccessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace MenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+        }
+        
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void grayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void orangeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void aboutUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This project is developed by Ozlem SARITAS and Mehmet KIROGLU!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Cef.Shutdown();
+            this.Close();
+        }
+
+        private void rambo2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=WQGJAIYtWD4");
+            label1.Text = "Rambo 2";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //webBrowser1.Navigate("https://www.google.com");
+        }
+    }
+}
